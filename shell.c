d0a263fdadb9daf9cd952dc41952d07cb5d86945
Velu Manohar <velumanohar2000@gmail.com>
2022-09-24 19:37:01 +0000

    works with no input, cd, quit

--- shell.c
+++ shell.c
@@ -43,71 +43,82 @@
 int main()
 {
 
-  char * command_string = (char*) malloc( MAX_COMMAND_SIZE );
+    char s[100];
+    char *command_string = (char *)malloc(MAX_COMMAND_SIZE);
 
-  while( 1 )
-  {
-    // Print out the msh prompt
-    printf ("msh> ");
-
-    // Read the command from the commandline.  The
-    // maximum command that will be read is MAX_COMMAND_SIZE
-    // This while command will wait here until the user
-    // inputs something since fgets returns NULL when there
-    // is no input
-    while( !fgets (command_string, MAX_COMMAND_SIZE, stdin) );
-
-    /* Parse input */
-    char *token[MAX_NUM_ARGUMENTS];
-
-    int   token_count = 0;                                 
-                                                           
-    // Pointer to point to the token
-    // parsed by strsep
-    char *argument_ptr;                                         
-                                                           
-    char *working_string  = strdup( command_string );                
-
-    // we are going to move the working_string pointer so
-    // keep track of its original value so we can deallocate
-    // the correct amount at the end
-    char *head_ptr = working_string;
-
-    // Tokenize the input strings with whitespace used as the delimiter
-    while ( ( (argument_ptr = strsep(&working_string, WHITESPACE ) ) != NULL) && 
-              (token_count<MAX_NUM_ARGUMENTS))
+    while (1)
     {
-      token[token_count] = strndup( argument_ptr, MAX_COMMAND_SIZE );
-      if( strlen( token[token_count] ) == 0 )
-      {
-        token[token_count] = NULL;
-      }
-        token_count++;
-    }
+        // Print out the msh prompt
+        printf("msh  %s> ",getcwd(s,100));
 
-    // Now print the tokenized input as a debug check
-    // \TODO Remove this code and replace with your shell functionality
+        // Read the command from the commandline.  The
+        // maximum command that will be read is MAX_COMMAND_SIZE
+        // This while command will wait here until the user
+        // inputs something since fgets returns NULL when there
+        // is no input
+        while (!fgets(command_string, MAX_COMMAND_SIZE, stdin))
+            ;
 
-    int token_index  = 0;
-    for( token_index = 0; token_index < token_count; token_index ++ ) 
-    {
-      printf("token[%d] = %s\n", token_index, token[token_index] );  
-    }
+        /* Parse input */
+        char *token[MAX_NUM_ARGUMENTS];
 
-    free( head_ptr );
+        int token_count = 0;
 
-    //TODO: change directories chdir() 
-    if (!(strcmp("cd", token[0])))
-    {
-        printf("Change directory to %s\n",token[1]);
-    }
+        // Pointer to point to the token
+        // parsed by strsep
+        char *argument_ptr;
 
+        char *working_string = strdup(command_string);
 
-    else if ((!(strcmp("quit", token[0]))) || !(strcmp("exit", token[0])))
-    {
+        // we are going to move the working_string pointer so
+        // keep track of its original value so we can deallocate
+        // the correct amount at the end
+        char *head_ptr = working_string;
+
+        // Tokenize the input strings with whitespace used as the delimiter
+        while (((argument_ptr = strsep(&working_string, WHITESPACE)) != NULL) &&
+               (token_count < MAX_NUM_ARGUMENTS))
+        {
+            token[token_count] = strndup(argument_ptr, MAX_COMMAND_SIZE);
+            if (strlen(token[token_count]) == 0)
+            {
+                token[token_count] = NULL;
+            }
+            token_count++;
+        }
+
+        // Now print the tokenized input as a debug check
+        // \TODO Remove this code and replace with your shell functionality
+
+        int token_index = 0;
+        for (token_index = 0; token_index < token_count; token_index++)
+        {
+            printf("token[%d] = %s\n", token_index, token[token_index]);
+        }
+
+        free(head_ptr);
+
+        // TODO: if no input then program will continue
+        if (token[0] == NULL)
+        {
+            continue;
+        }   
+
+        //when user inputs quit
+        else if ((!(strcmp("quit", token[0]))) || !(strcmp("exit", token[0])))
+        {
             printf("Goodbye\n");
             return 0;
-    }
+        }
+        
+        //when user inputs cd
+        else if (!(strcmp("cd", token[0])))
+        {
+           if (chdir(token[1]) != 0)
+           {
+               printf("chdir(%s) failed\n", token[1]);
+           }
+        }
   }
   return 0;
   // e2520ca2-76f3-90d6-0242ac120003
